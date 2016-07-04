c05ef0d8e4c380b1be0974865c53b8c07d93b382
KLSimmons <klsimmons88@gmail.com>
2016-07-04 11:22:48 -0400

    Create count_by_brand method

--- lib/analyzable.rb
+++ lib/analyzable.rb
@@ -7,4 +7,16 @@ module Analyzable
     end
     (price /= products.length).round(2)
   end
+
+  def print_report(products)
+    # TODO put avg price, counts by brand, counts by product name
+    return "#{average_price(products)}"
+  end
+
+  def count_by_brand(products)
+    brand_hash = {}
+    count = 0
+    products.each { |product| brand_hash[product.brand] = (count + 1) }
+    return brand_hash
+  end
 end
